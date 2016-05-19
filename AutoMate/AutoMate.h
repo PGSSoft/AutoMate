4db96346adffaeae857e55689756ed9779a86450
Joanna Bednarz <jbednarz@pgs-soft.com>
2016-05-19 08:19:47 +0200

    Initial Commit

---
+++ AutoMate/AutoMate.h
@@ -0,0 +1,19 @@
+//
+//  AutoMate.h
+//  AutoMate
+//
+//  Created by Joanna Bednarz on 19/05/16.
+//  Copyright © 2016 PGS Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AutoMate.
+FOUNDATION_EXPORT double AutoMateVersionNumber;
+
+//! Project version string for AutoMate.
+FOUNDATION_EXPORT const unsigned char AutoMateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoMate/PublicHeader.h>
+
+
