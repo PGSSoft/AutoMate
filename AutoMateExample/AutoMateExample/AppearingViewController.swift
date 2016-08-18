e3c1d05c2c4aed73cb451e189ebdc3dcc65d7ac4
Pawel Szot <pszot@pgs-soft.com>
2016-08-18 13:41:19 +0200

    PAI-65 add remaining tests

--- AutoMateExample/AutoMateExample/AppearingViewController.swift
+++ AutoMateExample/AutoMateExample/AppearingViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class AppearingViewController: UIViewController {
     // MARK: IBOutlets
-    @IBOutlet var button: UIButton!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet var notExistingButton: UIButton!
 
     // MARK: View lifecycle
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         button.hidden = true
+
+        notExistingButton.removeFromSuperview()
     }
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         UIView.animateWithDuration(5.0, animations: { self.view.backgroundColor = UIColor.yellowColor() }, completion: { _ in
             self.button.hidden = false
+            self.view.addSubview(self.notExistingButton)
         })
     }
 }
