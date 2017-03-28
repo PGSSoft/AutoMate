c301da1e47fc82e93deb3653c7450acba465ca98
Bartosz Janda <bjanda@pgs-soft.com>
2017-03-28 14:44:48 +0200

    PAI-98 Using json files

---
+++ AutoMate/Foundation extensions/Bundle.swift
@@ -0,0 +1,25 @@
+//
+//  Bundle.swift
+//  AutoMate
+//
+//  Created by Bartosz Janda on 28.03.2017.
+//  Copyright © 2017 PGS Software. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+
+    /// AutoMate Bundle Identifier
+    public class var autoMateIdentifier: String {
+        return "com.pgs-soft.AutoMate"
+    }
+
+    /// AutoMate framework bundle
+    public class var autoMate: Bundle {
+        guard let bundle = Bundle(identifier: autoMateIdentifier) else {
+            preconditionFailure("Cannot find allow button.")
+        }
+        return bundle
+    }
+}
--- AutoMate/Models/LocationAlerts.swift
+++ AutoMate/Models/LocationAlerts.swift
@@ -7,40 +7,12 @@ extension LocationAlertAllow {
 
     /// Represents all possible "allow" buttons in location service messages.
     public static var allow: [String] {
-        return [
-            "Allow",
-            "Autoriser",
-            "Benarkan",
-            "Cho phép",
-            "Consenti",
-            "Dozvoli",
-            "Engedélyezés",
-            "Erlauben",
-            "Izinkan",
-            "İzin Ver",
-            "Permetre",
-            "Permiteți",
-            "Permitir",
-            "Povolit",
-            "Povoliť",
-            "Pozwalaj",
-            "Salli",
-            "Sta toe",
-            "Tillad",
-            "Tillat",
-            "Tillåt",
-            "Να επιτρέπεται",
-            "Дозволити",
-            "Разрешить",
-            "אפשר",
-            "السماح",
-            "अनुमति दें",
-            "อนุญาต",
-            "허용",
-            "允許",
-            "允许",
-            "許可",
-        ]
+        guard let url = Bundle.autoMate.url(forResource: "LocationAlertAllow", withExtension: "json"),
+            let data = try? Data(contentsOf: url),
+            let json = (try? JSONSerialization.jsonObject(with: data)) as? [String: [String]] else {
+            return []
+        }
+        return json.flatMap { $0.value }
     }
 }
 
@@ -48,38 +20,12 @@ extension LocationAlertCancel {
 
     /// Represents all possible "cancel" buttons in location service messages.
     public static var cancel: [String] {
-        return [
-            "Abbrechen",
-            "Annuleer",
-            "Annuler",
-            "Annulla",
-            "Annuller",
-            "Anulați",
-            "Anuluj",
-            "Avbryt",
-            "Batal",
-            "Batalkan",
-            "Cancel",
-            "Cancelar",
-            "Cancel·lar",
-            "Hủy",
-            "Kumoa",
-            "Mégsem",
-            "Poništi",
-            "Vazgeç",
-            "Zrušit",
-            "Zrušiť",
-            "Ακύρωση",
-            "Отменить",
-            "Скасувати",
-            "ביטול",
-            "إلغاء",
-            "रद्द करें",
-            "ยกเลิก",
-            "취소",
-            "キャンセル",
-            "取消",
-        ]
+        guard let url = Bundle.autoMate.url(forResource: "LocationAlertCancel", withExtension: "json"),
+            let data = try? Data(contentsOf: url),
+            let json = (try? JSONSerialization.jsonObject(with: data)) as? [String: [String]] else {
+            return []
+        }
+        return json.flatMap { $0.value }
     }
 }
 
@@ -87,42 +33,12 @@ extension LocationAlertDeny {
 
     /// Represents all possible "deny" buttons in location service messages.
     public static var deny: [String] {
-        return [
-            "Älä salli",
-            "Don’t Allow",
-            "Ikke tillat",
-            "İzin Verme",
-            "Jangan Benarkan",
-            "Não Permitir",
-            "Não permitir",
-            "Nein",
-            "Nemoj dozvoliti",
-            "Nepovoliť",
-            "Nie pozwalaj",
-            "No permetre",
-            "No permitir",
-            "Non consentire",
-            "Nu permiteți",
-            "Refuser",
-            "Sta niet toe",
-            "Tidak Boleh",
-            "Tillad ikke",
-            "Tillåt inte",
-            "Tiltás",
-            "Từ chối",
-            "Zakázat",
-            "Άρνηση",
-            "Заборонити",
-            "Запретить",
-            "אל תאפשר",
-            "عدم السماح",
-            "अनुमति न दें",
-            "ไม่อนุญาต",
-            "허용 안 함",
-            "不允許",
-            "不允许",
-            "許可しない",
-        ]
+        guard let url = Bundle.autoMate.url(forResource: "LocationAlertDeny", withExtension: "json"),
+            let data = try? Data(contentsOf: url),
+            let json = (try? JSONSerialization.jsonObject(with: data)) as? [String: [String]] else {
+            return []
+        }
+        return json.flatMap { $0.value }
     }
 }
 
@@ -130,19 +46,12 @@ extension LocationAlertOk {
 
     /// Represents all possible "ok" buttons in location service messages.
     public static var ok: [String] {
-        return [
-            "OK",
-            "OKE",
-            "Tamam",
-            "ΟΚ",
-            "ОК",
-            "אישור",
-            "موافق",
-            "ठीक",
-            "ตกลง",
-            "확인",
-            "好",
-        ]
+        guard let url = Bundle.autoMate.url(forResource: "LocationAlertOk", withExtension: "json"),
+            let data = try? Data(contentsOf: url),
+            let json = (try? JSONSerialization.jsonObject(with: data)) as? [String: [String]] else {
+            return []
+        }
+        return json.flatMap { $0.value }
     }
 }
 
@@ -174,44 +83,14 @@ extension LocationAlertOk {
 public struct LocationAlwaysAlert: SystemAlert, LocationAlertAllow, LocationAlertDeny {
 
     /// Represents all possible messages in `LocationAlwaysAlert` service alert.
-    public static let messages = [
-        "Allow “*” to access your location even when you are not using the app?",
-        "Autoriser « * » à accéder à vos données de localisation même lorsque vous n’utilisez pas l’app ?",
-        "Autoriser « * » à accéder à vos données de localisation même si vous n’utilisez pas l’app?",
-        "Benarkan “*” mengakses lokasi anda walaupun anda tidak menggunakan aplikasi?",
-        "Chcete aplikaci * povolit přístup k polohovým údajům i v čase, kdy ji nepoužíváte?",
-        "Chcete povoliť aplikácii „*“ pristupovať k vašej polohe v čase, keď ju nepoužívate?",
-        "Cho phép “*” truy cập vị trí của bạn ngay cả khi bạn không dùng ứng dụng?",
-        "Consenti a “*” di accedere ai dati relativi alla tua posizione anche quando non stai usando l'app?",
-        "Darf „*“ auf deinen Standort zugreifen, selbst wenn du die App nicht benutzt?",
-        "Dozvoliti aplikaciji “*” pristup vašoj lokaciji čak i kad ne koristite aplikaciju?",
-        "Engedélyezi, hogy a(z) „*” hozzáférjen az Ön helyzetéhez akkor is, ha nem használja az alkalmazást?",
-        "Izinkan “*” untuk mengakses lokasi Anda bahkan saat Anda tidak menggunakan app?",
-        "Må “*” bruge din lokalitet, også når du ikke bruger appen?",
-        "Permets que “*” accedeixi a la teva ubicació encara que no estiguis utilitzant l’aplicació?",
-        "Permiteți ca aplicația “*” să acceseze localizarea dvs. chiar și atunci când nu o utilizați?",
-        "Permitir que “*” aceda à sua localização mesmo que não esteja a usar a aplicação?",
-        "Permitir que “*” tenha acesso à sua localização mesmo quando você não estiver usando o aplicativo?",
-        "Sallitko, että * käyttää sijaintiasi myös silloin, kun et käytä appia?",
-        "Udostępniać programowi „*” Twoje położenie także, gdy go nie używasz?",
-        "Vil du gi «*» tilgang til plasseringen din også når du ikke bruker appen?",
-        "Vill du tillåta ”*” att se din platsinfo även när du inte använder appen?",
-        "Wil je * toegang tot je locatie toestaan, zelfs als je de app niet gebruikt?",
-        "¿Permitir a * acceder a tu ubicación aunque no estés utilizando la aplicación?",
-        "Να επιτρέπεται στην εφαρμογή «*» η πρόσβαση στην τοποθεσία σας ακόμη κι όταν δεν χρησιμοποιείτε την εφαρμογή;",
-        "Дозволити «*» отримувати ваше місце, навіть коли програма не використовуєтьcя?",
-        "Разрешить программе «*» доступ к Вашей геопозиции, даже когда Вы не работаете с этой программой?",
-        "לאפשר ל-״*״ לגשת לפרטי מיקומך גם כשאינך משתמש/ת ביישום?",
-        "السماح لـ \"*\" بالوصول إلى الموقع الخاص بك حتى عند عدم استخدامك للتطبيق؟",
-        "ऐप के न उपयोग होने के दौरान भी “*” को अपने वर्तमान स्थान का उपयोग करने दें?",
-        "อนุญาตให้ “*” เข้าถึงตำแหน่งที่ตั้งของคุณแม้ว่าคุณจะไม่ได้ใช้แอพอยู่หรือไม่",
-        "이 앱을 사용하지 않을 때에도 ‘*’이(가) 사용자의 위치에 접근하도록 허용하겠습니까?",
-        "“*”, uygulamayı kullanmıyorken bile konumunuza erişebilsin mi?",
-        "“*”を使用していないときでも位置情報の利用を許可しますか?",
-        "允许“*”在您并未使用该应用时访问您的位置吗？",
-        "要允許「*」在你並非使用 App 時也能取用你的位置嗎？",
-        "要允許「*」在您未使用 App 時也可取用您的位置嗎？",
-    ]
+    public static var messages: [String] {
+        guard let url = Bundle.autoMate.url(forResource: "LocationAlwaysAlert", withExtension: "json"),
+            let data = try? Data(contentsOf: url),
+            let json = (try? JSONSerialization.jsonObject(with: data)) as? [String: [String]] else {
+            return []
+        }
+        return json.flatMap { $0.value }
+    }
 
     /// System service alert element.
     public var alert: XCUIElement
@@ -256,44 +135,14 @@ public struct LocationAlwaysAlert: SystemAlert, LocationAlertAllow, LocationAler
 public struct LocationUpgradeWhenInUseAlwaysAlert: SystemAlert, LocationAlertAllow, LocationAlertCancel {
 
     /// Represents all possible messages in `LocationUpgradeWhenInUseAlwaysAlert` service alert.
-    public static let messages = [
-        "Allow “*” to also access your location even when you are not using the app?",
-        "Autoriser « * » à accéder aussi à vos données de localisation lorsque vous n’utilisez pas l’app ?",
-        "Autoriser « * » à accéder aussi à vos données de localisation même si vous n’utilisez pas l’app?",
-        "Benarkan “*” turut mengakses lokasi anda walaupun anda tidak menggunakan aplikasi?",
-        "Chcete aplikaci * povolit přístup k polohovým údajům i v čase, kdy ji nepoužíváte?",
-        "Chcete povoliť aplikácii „*“ pristupovať k vašej polohe aj v čase, keď ju nepoužívate?",
-        "Consenti a “*” di accedere anche ai dati relativi alla tua posizione quando l'app non è in uso?",
-        "Cũng cho phép “*” truy cập vị trí của bạn ngay cả khi bạn không dùng ứng dụng?",
-        "Darf „*“ auch auf deinen Standort zugreifen, wenn du die App nicht benutzt?",
-        "Dozvoliti aplikaciji “*” da pristupa vašoj lokaciji čak i kad ne koristite aplikaciju?",
-        "Engedélyezi, hogy a(z) „*” hozzáférjen az Ön helyzetéhez akkor is, ha nem használja az alkalmazást?",
-        "Izinkan juga “*” untuk mengakses lokasi Anda bahkan saat Anda tidak menggunakan app?",
-        "Må “*” også få adgang til din lokalitet, når du ikke bruger appen?",
-        "Permets que “*” també accedeixi a la teva ubicació encara que no estiguis utilitzant l’aplicació?",
-        "Permiteți ca aplicația “*” să acceseze, de asemenea, localizarea dvs. chiar și atunci când nu o utilizați?",
-        "Permitir que “*” também aceda à sua localização quando não está a usar a aplicação?",
-        "Permitir que “*” também tenha acesso à sua localização mesmo quando você não estiver usando o aplicativo?",
-        "Sallitko, että * käyttää sijaintiasi myös silloin, kun et käytä appia?",
-        "Udostępniać programowi „*” Twoje położenie także, gdy go nie używasz?",
-        "Vil du også gi «*» tilgang til plasseringen din selv når du ikke bruker appen?",
-        "Vill du tillåta att ”*” även ser din platsinfo när du inte använder appen?",
-        "Wil je * zelfs toegang tot je locatie toestaan als je de app niet gebruikt?",
-        "¿Permitir a * acceder también a tu ubicación aunque no estés utilizando la aplicación?",
-        "Να επιτρέπεται επίσης στην εφαρμογή «*» η πρόσβαση στην τοποθεσία σας ακόμη κι όταν δεν χρησιμοποιείτε την εφαρμογή;",
-        "Дозволити «*» також отримувати ваше місце, навіть коли програма не використовуєтьcя?",
-        "Разрешить программе «*» доступ к Вашей геопозиции, даже когда Вы не работаете с этой программой?",
-        "לאפשר גם ל-״*״ לגשת לפרטי מיקומך אפילו כשאינך משתמש/ת ביישום?",
-        "السماح لـ \"*\" أيضًا بالوصول إلى الموقع الخاص بك حتى عند عدم استخدامك للتطبيق؟",
-        "ऐप के उपयोग में न होने के दौरान भी “*” को अपने स्थान का उपयोग करने की अनुमति दें?",
-        "อนุญาตให้ “*” เข้าถึงตำแหน่งที่ตั้งของคุณด้วยแม้ว่าคุณจะไม่ได้ใช้แอพอยู่หรือไม่",
-        "‘*’을(를) 사용하지 않을 때에도 해당 앱이 사용자의 위치에 접근하도록 허용하곘습니까?",
-        "“*”, uygulamayı kullanmıyorken bile konumunuza da erişebilsin mi?",
-        "“*”を使用していないときでも位置情報の利用を許可しますか?",
-        "允许“*”在您并未使用该应用时访问您的位置吗？",
-        "要允許「*」在你並非使用 App 時也能取用你的位置嗎？",
-        "要允許「*」在您未使用 App 時也可取用您的位置嗎？",
-    ]
+    public static var messages: [String] {
+        guard let url = Bundle.autoMate.url(forResource: "LocationUpgradeWhenInUseAlwaysAlert", withExtension: "json"),
+            let data = try? Data(contentsOf: url),
+            let json = (try? JSONSerialization.jsonObject(with: data)) as? [String: [String]] else {
+            return []
+        }
+        return json.flatMap { $0.value }
+    }
 
     /// System service alert element.
     public var alert: XCUIElement
@@ -338,44 +187,14 @@ public struct LocationUpgradeWhenInUseAlwaysAlert: SystemAlert, LocationAlertAll
 public struct LocationWhenInUseAlert: SystemAlert, LocationAlertAllow, LocationAlertDeny {
 
     /// Represents all possible messages in `LocationWhenInUseAlert` service alert.
-    public static let messages = [
-        "A(z) „*” hozzáférhet a helyzetéhez az alkalmazás használatakor?",
-        "Allow “*” to access your location while you use the app?",
-        "Autoriser « * » à accéder à vos données de localisation lorsque vous utilisez cette app?",
-        "Autoriser « * » à accéder à vos données de localisation lorsque vous utilisez l’app ?",
-        "Benarkan “*” mengakses lokasi anda semasa anda menggunakan aplikasi?",
-        "Chcete aplikaci * povolit přístup k polohovým údajům v čase, kdy ji používáte?",
-        "Chcete povoliť aplikácii „*“ pristupovať k vašej polohe v čase, keď ju používate?",
-        "Cho phép “*” truy cập vị trí của bạn khi bạn dùng ứng dụng?",
-        "Consenti a “*” di accedere ai dati relativi alla tua posizione quando stai usando l'app?",
-        "Darf „*“ auf deinen Standort zugreifen, wenn du die App benutzt?",
-        "Dozvoliti aplikaciji “*” pristup vašoj lokaciji dok koristite aplikaciju?",
-        "Izinkan “*” untuk mengakses lokasi Anda saat Anda menggunakan app?",
-        "Må “*” bruge din lokalitet, når du bruger appen?",
-        "Permets que “*” accedeixi a la teva ubicació mentre utilitzes l’aplicació?",
-        "Permiteți ca aplicația “*” să acceseze localizarea dvs. în timp ce o utilizați?",
-        "Permitir que “*” aceda à sua localização enquanto usa a aplicação?",
-        "Permitir que “*” tenha acesso à sua localização enquanto você usa o aplicativo?",
-        "Sallitko, että * käyttää sijaintiasi silloin, kun käytät appia?",
-        "Udostępniać programowi „*” Twoje położenie, gdy go używasz?",
-        "Vil du gi «*» tilgang til plasseringen din når du bruker appen?",
-        "Vill du tillåta ”*” att se din platsinfo när du använder appen?",
-        "Wil je * toegang tot je locatie toestaan terwijl je de app gebruikt?",
-        "¿Permitir a * acceder a tu ubicación mientras utilizas la aplicación?",
-        "Να επιτρέπεται στην εφαρμογή «*» η πρόσβαση στην τοποθεσία σας κατά τη χρήση της;",
-        "Дозволити «*» отримувати ваше місце, коли ви використовуєте програму?",
-        "Разрешить программе «*» доступ к Вашей геопозиции, пока Вы работаете с этой программой?",
-        "לאפשר ל-״*״ לגשת לפרטי מיקומך בעת שימושך ביישום?",
-        "السماح لـ \"*\" بالوصول إلى الموقع الخاص بك عند استخدامك للتطبيق؟",
-        "ऐप के उपयोग होने के दौरान “*” को अपने वर्तमान स्थान का उपयोग करने दें?",
-        "อนุญาตให้ “*” เข้าถึงตำแหน่งที่ตั้งของคุณระหว่างที่คุณใช้แอพหรือไม่",
-        "앱을 사용하는 동안 ‘*’에서 사용자의 위치에 접근하도록 허용하겠습니까?",
-        "“*”, uygulamayı kullanırken konumunuza erişebilsin mi?",
-        "“*”の使用中に位置情報の利用を許可しますか?",
-        "允许“*”在您使用该应用时访问您的位置吗？",
-        "要允許「*」在你正在使用 App 時取用你的位置嗎？",
-        "要允許「*」在您使用 App 時取用您的位置嗎？",
-    ]
+    public static var messages: [String] {
+        guard let url = Bundle.autoMate.url(forResource: "LocationWhenInUseAlert", withExtension: "json"),
+            let data = try? Data(contentsOf: url),
+            let json = (try? JSONSerialization.jsonObject(with: data)) as? [String: [String]] else {
+            return []
+        }
+        return json.flatMap { $0.value }
+    }
 
     /// System service alert element.
     public var alert: XCUIElement
