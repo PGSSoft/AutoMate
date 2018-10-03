3338566d8e79718c278d6439cff19e3c2cca5625
Joanna Bednarz <>
2018-10-03 20:44:28 +0200

    Bump Swift version and Xcode 10 everywhere. Add support for new device sizes (XR, XS, XS Max). Fix HealthKit permission tests.

--- AutoMateExample/AutoMateExampleUITests/PermissionsTests.swift
+++ AutoMateExample/AutoMateExampleUITests/PermissionsTests.swift
@@ -162,19 +162,6 @@ class PermissionsTests: AppUITestCase {
         if app.isRunningOnIpad {
             return
         }
-        var handled = false
-        let token = addUIInterruptionMonitor(withDescription: "HomeKit") { (alert) -> Bool in
-            guard let homeKitAlert = HealthAuthorizationDontAllowAlert(element: alert) else {
-                XCTFail("Cannot create HealthAuthorizationDontAllowAlert object")
-                return false
-            }
-
-            XCTAssertTrue(homeKitAlert.okElement.exists)
-
-            homeKitAlert.okElement.tap()
-            handled = true
-            return true
-        }
 
         mainPage.goToPermissionsPageMenu()
         permissionsPage.goToHealthKit()
@@ -191,10 +178,16 @@ class PermissionsTests: AppUITestCase {
         healthPermissionPage.denyElement.tap()
         Thread.sleep(forTimeInterval: 2)
 
+        guard let healthAlert = HealthAuthorizationDontAllowAlert(element: app.alerts.firstMatch) else {
+            XCTFail("Cannot create HealthAuthorizationDontAllowAlert object")
+            return
+        }
+
+        XCTAssertTrue(healthAlert.okElement.exists)
+
+        healthAlert.okElement.tap()
         healthKitPage.goBack()
         permissionsPage.goBack()
-        removeUIInterruptionMonitor(token)
-        XCTAssertTrue(handled)
     }
 
     func testSpeechRecognitionAlert() {
